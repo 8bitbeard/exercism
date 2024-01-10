fa576ac76a2e2c5df0c6d5afc96b67f544463ee1
Wilton Souza <willsouzafilho@gmail.com>
2024-01-10 08:23:33 -0300

    #2 Golang chessboard exercise

--- go/chessboard/chessboard.go
+++ go/chessboard/chessboard.go
@@ -1,7 +1,7 @@
 package chessboard
 
 // Declare a type named File which stores if a square is occupied by a piece - this will be a slice of bools
-type File []bool
+type File [8]bool
 
 // Declare a type named Chessboard which contains a map of eight Files, accessed with keys from "A" to "H"
 type Chessboard map[string]File
