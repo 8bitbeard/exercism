d56226db5dc2cb420622612e2ef89af952f8067e
Wilton Souza <willsouzafilho@gmail.com>
2024-01-14 12:09:59 -0300

    Golang elons toys exercise

---
+++ go/elons-toys/elons_toys.go
@@ -0,0 +1,31 @@
+package elon
+
+import "fmt"
+
+// TODO: define the 'Drive()' method
+func (c *Car) Drive() {
+	if c.battery-c.batteryDrain >= 0 {
+		c.distance += c.speed
+		c.battery -= c.batteryDrain
+	}
+}
+
+// TODO: define the 'DisplayDistance() string' method
+func (c Car) DisplayDistance() string {
+  return fmt.Sprintf("Driven %d meters", c.distance)
+}
+
+// TODO: define the 'DisplayBattery() string' method
+func (c Car) DisplayBattery() string {
+  return fmt.Sprintf("Battery at %d%%", c.battery)
+}
+
+// TODO: define the 'CanFinish(trackDistance int) bool' method
+func (c Car) CanFinish(trackDistance int) bool {
+  turns := trackDistance / c.speed
+  if trackDistance % c.speed != 0 {
+    turns++
+  }
+
+  return turns * c.batteryDrain <= c.battery
+}
