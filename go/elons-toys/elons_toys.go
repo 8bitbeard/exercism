b4d4fc00e0b70d46008f358d6a916b24f54e30cc
Wilton Souza <willsouzafilho@gmail.com>
2024-01-14 15:08:13 -0300

    Golang elons toys exercise #2

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -10,18 +10,18 @@ func (c *Car) Drive() {
 }
 
 func (c Car) DisplayDistance() string {
-  return fmt.Sprintf("Driven %d meters", c.distance)
+	return fmt.Sprintf("Driven %d meters", c.distance)
 }
 
 func (c Car) DisplayBattery() string {
-  return fmt.Sprintf("Battery at %d%%", c.battery)
+	return fmt.Sprintf("Battery at %d%%", c.battery)
 }
 
 func (c Car) CanFinish(trackDistance int) bool {
-  turns := trackDistance / c.speed
-  if trackDistance % c.speed != 0 {
-    turns++
-  }
+	turns := trackDistance / c.speed
+	if trackDistance%c.speed != 0 {
+		turns++
+	}
 
-  return turns * c.batteryDrain <= c.battery
+	return turns*c.batteryDrain <= c.battery
 }
